8bbbde30638e4bfa65ae09ada4cd66c3dae6340b
Emir <eux202@gmail.com>
2023-03-02 23:27:49 +0300

    image detail in builds page changed

--- components/ImageDetail.tsx
+++ components/ImageDetail.tsx
@@ -24,7 +24,7 @@ export default function ImageDetail({ width, height, src, details }: ImageDetail
           style={{ left: `${detail.left}`, top: `${detail.top}` }}
           className={`select-none absolute text-white py-2 flex items-center px-4 group-hover:opacity-100 opacity-0 transition-all duration-500 bg-13/[.7]
            border-[1px] border-white/[.15] rounded-lg`}>
-          {detail.name} <RiArrowRightSLine className="text-xl" />
+          {detail.name}
           <span className="-bottom-8 absolute left-0 w-6 h-6 rounded-full bg-white/[.7]"></span>
         </span>
       ))}
