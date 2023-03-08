9721ff1d163d6ae6b608fce43b82bfcf8906c047
Emir <eux202@gmail.com>
2023-03-08 16:11:45 +0300

    icons file created

---
+++ lib/icons.tsx
@@ -0,0 +1,24 @@
+export function DownArrow(props) {
+  return (
+    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M8.96004 1.47485L5.70004 4.73485C5.31504 5.11985 4.68504 5.11985 4.30004 4.73485L1.04004 1.47485"
+        stroke="currentColor"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function LinkArrow(props) {
+  return (
+    <svg width="7" height="6" viewBox="0 0 7 6" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M6.47465 0.525106C6.47465 0.248964 6.25079 0.0251062 5.97465 0.0251062H1.47465C1.19851 0.0251062 0.97465 0.248964 0.97465 0.525106C0.97465 0.801249 1.19851 1.02511 1.47465 1.02511H5.47465V5.02511C5.47465 5.30125 5.69851 5.52511 5.97465 5.52511C6.25079 5.52511 6.47465 5.30125 6.47465 5.02511V0.525106ZM1.37846 5.82841L6.3282 0.87866L5.6211 0.171553L0.671349 5.1213L1.37846 5.82841Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
